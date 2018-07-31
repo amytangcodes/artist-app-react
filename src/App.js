950af9b62234ed6598ad059ca5e275a3f0186b0a
Amy Tang <amy@pathfactory.com>
2018-07-31 12:04:39 -0400

    Adding and updating artist

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import { getArtists } from "./repo";
+import { getArtists, addArtist } from "./repo";
 
 class App extends Component {
-  state = { artists: null, artist_name: "" };  // Initial state
+  state = { artists: null, artist_name: "" }; // Initial state
 
   componentDidMount() {
     getArtists().then(artists => {
       this.setState({ artists: artists });
-    }); 
+    });
   }
 
-  handleChange = (event) => {
-    this.setState({artist_name: event.target.value});
-  }
+  handleChange = event => {
+    this.setState({ artist_name: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    addArtist({ artist_name: this.state.artist_name }).then(artist => {
+      this.setState({ artists: this.state.artists.concat(artist) });
+    });
+  };
 
   render() {
     const { artists, artist_name } = this.state; // Destructuring
 
     return (
       <div className="App">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Artist Name</label>
-          <input type="text" name="artist_name" placeholder="Artist Name" value={artist_name} onChange={this.handleChange}/>
+          <input
+            type="text"
+            name="artist_name"
+            placeholder="Artist Name"
+            value={artist_name}
+            onChange={this.handleChange}
+          />
         </form>
         <ul className="App-intro">
           {artists
--- src/repo.js
+++ src/repo.js
@@ -3,6 +3,15 @@ import "whatwg-fetch";
 export const getArtists = () =>
   fetch("http://localhost:3000/artists").then(response => response.json());
 
+export const addArtist = artist =>
+  fetch("http://localhost:3000/artists", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(artist)
+  }).then(response => response.json());
+
 //  const foo = input => {
 //    // other stuff
 //    return { bar: "baz" };
