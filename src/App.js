da2654a25b51d63581e88d9aa12f8a44fa18dd3b
Amy Tang <amy@pathfactory.com>
2018-07-31 11:13:51 -0400

    Added artist id and name

--- src/App.js
+++ src/App.js
@@ -4,23 +4,47 @@ import "./App.css";
 import { getArtists } from "./repo";
 
 class App extends Component {
+  state = { artists: null };
+
   componentDidMount() {
-    getArtists().then(args => console.log(args)); // returns a promise
+    getArtists().then(artists => {
+      this.setState({ artists });
+    }); // returns a promise
   }
 
   render() {
+    console.log(this.state);
+    const { artists } = this.state; // Destructuring
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          {artists
+            ? artists.map(artist => (
+                <div key={artist.id}> {artist.artist_name} </div>
+              ))
+            : "Loading"}
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+// Destructuring
+// const obj = { a: 1, b: 1 };
+
+// const a = obj.a;
+// const b = obj.b;
+
+// const { a, b } = obj;
+
+// const a = obj.a;
+// const bee = obj.b;
+
+// const { a, b: bee } = obj;
